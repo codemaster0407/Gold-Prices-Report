63ed2637dead7ae32458a50f021a124a5d4ac152
codemaster0407 <chaitanyamu24@gmail.com>
2025-08-08 20:00:41 +0530

    fix: remove sandbox

--- scrape_data.py
+++ scrape_data.py
@@ -106,6 +106,10 @@ def initiate_driver(url = config.URL):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-sandbox")  
+    options.add_argument("--disable-dev-shm-usage")
+
+
     driver = webdriver.Chrome(options=options)
     
     stealth(driver,
